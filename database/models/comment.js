bb8e1d4daf0a06c946ce631c4d9e8377f5966b3b
Jordan Schroter <jordan.schroter@gmail.com>
2017-10-16 20:43:17 -0400

    remove noisy jsdoc

--- database/models/comment.js
+++ database/models/comment.js
@@ -15,60 +15,12 @@ const likes = db => db.raw('count(case direction when 1 then 1 else null end) as
 const dislikes = db => db.raw('count(case direction when -1 then 1 else null end) as dislikes')
 const score = db => db.raw('coalesce(sum(direction), 0) as score')
 
-/**
- * @typedef Comment
- * @type {object}
- * @property {number} id - the comment id
- * @property {string} text - the text of the comment
- * @property {string} proposalHash - the proposal of the comment
- * @property {number} createdBy - the user that created the comment
- * @property {string} createdAt - the date the comment was created
- * @property {string} updatedAt - the date the comment was last updated
- */
-
-/**
- * @typedef CommentAttributes
- * @type {object}
- * @property {string} text
- * @property {string} proposalHash
- * @property {number} createdBy
- */
-
-/**
- * @typedef CommentVote
- * @type {object}
- * @property {number} direction - the user's vote (1 = like, -1 = dislike, 0 = unvote)
- * @property {number} createdBy - the user of the vote
- * @property {string} createdAt - the date the vote was created
- * @property {string} updatedAt - the date the vote was last updated
- */
-
-/**
- * @typedef CommentVoteAttributes
- * @type {object}
- * @property {number} direction - the user's vote (1 = like, -1 = dislike, 0 = unvote)
- * @property {number} createdBy - the user of the vote
- */
-
-/**
- * @function create - creates a new comment
- * @param {CommentAttributes} attrs - the attributes of the comment to create
- * @param {*=} trx - the optional transaction context
- * @return {Promise.<Comment>} - the newly created comment
- */
 module.exports.create = async (attrs, trx) => {
 	const db = await getDbDriver()
 	const [comment] = await withTransaction(trx, db(table).insert(createTimestamps(attrs), '*'))
 	return comment
 }
 
-/**
- * @function update - updates an existing comment
- * @param {number} id - the id of the comment to update
- * @param {CommentAttributes} attrs - the attributes of the comment to update
- * @param {*=} trx - the optional transaction context
- * @return {Promise.<Comment>} - the updated comment
- */
 module.exports.update = async (id, attrs, trx) => {
 	const db = await getDbDriver()
 	const [comment] = await withTransaction(
@@ -81,12 +33,6 @@ module.exports.update = async (id, attrs, trx) => {
 	return comment
 }
 
-/**
- * @function - soft deletes a comment
- * @param {number} id - the id of the comment to delete
- * @param {*=} trx - the optional transaction context
- * @return {Promise<?Comment>} - the updated comment
- */
 module.exports.delete = async (id, trx) => {
 	const db = await getDbDriver()
 	const [comment] = await withTransaction(
@@ -99,11 +45,6 @@ module.exports.delete = async (id, trx) => {
 	return comment
 }
 
-/**
- * @function findOne - finds an existing comment that matches the provided attributes
- * @param {CommentAttributes} attrs - the attributes of the comment to match against
- * @return {Promise.<?Comment>} - the matched comment, null if not found
- */
 module.exports.findOne = async attrs => {
 	const db = await getDbDriver()
 	const comment = await db
@@ -117,11 +58,6 @@ module.exports.findOne = async attrs => {
 	return comment
 }
 
-/**
- * @function findAll - finds all existing comments that match the provided attributes
- * @param {CommentAttributes} attrs - the attributes of the comment to match against
- * @return {Promise.<Comment[]>} - the matched comments
- */
 module.exports.findAll = async attrs => {
 	const db = await getDbDriver()
 	// purposefully returning deleted comments to preserve thread
@@ -135,11 +71,6 @@ module.exports.findAll = async attrs => {
 	return comments
 }
 
-/**
- * @function vote - insert or update a users vote for a comment
- * @param {CommentVoteAttributes} voteAttrs - the attributes of the vote
- * @return {Promise.<CommentVote>} - the comment vote
- */
 module.exports.vote = async (id, voteAttrs) => {
 	const { createdBy, direction } = voteAttrs
 	const db = await getDbDriver()
@@ -166,11 +97,6 @@ module.exports.vote = async (id, voteAttrs) => {
 	return vote
 }
 
-/**
- * @function toJSON - strips private fields for public consumption
- * @param {Comment} comment - the comment
- * @return {Comment} - the comment without private fields
- */
 module.exports.toJSON = comment => {
 	const json = omit(comment, privateFields)
 
